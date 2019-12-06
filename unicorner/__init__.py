7b30764935ee625792604048980c96d5f7fda9a7
Jazeps Basko <jazeps.basko@gmail.com>
2019-12-06 01:42:03 +0000

    Bump version: 0.3.0 → 0.3.1

--- unicorner/__init__.py
+++ unicorner/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Jazeps Basko"""
 __email__ = "jazeps.basko@gmail.com"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from .env import UnicornerEnv
 from .season_page import SeasonParse
