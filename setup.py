7770e1f4398d922cc7b05fe5817be486d2f9a625
Jazeps Basko <jazeps.basko@gmail.com>
2019-12-06 01:29:46 +0000

    Bump version: 0.2.4 → 0.3.0

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/zilupe/unicorner",
-    version="0.2.4",
+    version="0.3.0",
     zip_safe=False,
 )
--- unicorner/__init__.py
+++ unicorner/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Jazeps Basko"""
 __email__ = "jazeps.basko@gmail.com"
-__version__ = "0.2.4"
+__version__ = "0.3.0"
 
 from .env import UnicornerEnv
 from .season_page import SeasonParse
